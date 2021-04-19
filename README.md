4341d26d73f49e4455d293234232c23b300734ac
Sophia Stephenson <stephenson3@wisc.edu>
2021-04-18 20:16:33 -0500

    final upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Password Generator
+
+## Description
+Homework assignment 3 contains the following features:
+
+* This website will generate a random password depending on user input
+
+* The design should allow the user to fail gracefully, with a reset of the prompt sequence
+
+## Visuals
+
+See below for a preview of the web page:
+
+![The final webpage should appear as shown here](./assets/images/webpage-example.png)
+
+## Links
+
+Please find the deployed application here: https://stephenson36.github.io/HW3_SS/
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,57 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+    let lCaseTotal = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let uCaseTotal = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    let numbersTotal = [0,1,2,3,4,5,6,7,8,9];
+    let specialCharTotal = ['~','!','@','#','$','%','^','&','*','_','-','+','=',':',';','<','>','.','?']
+
+    let pLength = window.prompt('Choose length of password between 8 and 128 characters');
+    
+    if((pLength < 8) || (pLength > 128) || (isNaN(pLength))) {
+        return generatePassword();
+    }
+
+    let lCase = window.confirm('Include lowercase letters?');
+    let uCase = window.confirm('Include uppercase letters?');
+    let pNumbers = window.confirm('Include numeric values?');
+    let specialChar = window.confirm('Include special characters?');
+    
+    let passwordOptions = [];
+
+    if(lCase) {
+        passwordOptions = passwordOptions.concat(lCaseTotal);
+    }
+    if(uCase) {
+        passwordOptions = passwordOptions.concat(uCaseTotal);
+    }
+    if(pNumbers) {
+        passwordOptions = passwordOptions.concat(numbersTotal);
+    }
+    if(specialChar) {
+        passwordOptions = passwordOptions.concat(specialCharTotal);
+    }
+
+    let myPassword = '';
+    
+    if(passwordOptions.length === 0 ) {
+        window.alert('Must select at least one character type');
+        return generatePassword();
+    } else{
+        for(let i =0; i < pLength;i++) {
+            myPassword = myPassword.concat(randomByIndex(passwordOptions))
+        }
+    }
+
+    return myPassword;
+}
+
+function randomByIndex(myArray) {
+    let index = Math.floor(Math.random() * myArray.length);
+    return myArray[index];
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
